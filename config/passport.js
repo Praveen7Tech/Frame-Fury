17c4ab88f39342b139f9163d638e572b3d6d4232
Pr4veenn <praveen7.info@gmail.com>
2025-01-27 17:52:21 +0530

    production commit

--- config/passport.js
+++ config/passport.js
@@ -10,7 +10,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIEND_ID,
       clientSecret: process.env.GOOGLE_CLIEND_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: process.env.NODE_ENV ==='production' ? process.env.GOOGLE_AUTH_CALLBACK_URL:"/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
