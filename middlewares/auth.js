25c737c5d934d610ee3d67d272285adf0904e1c3
Pr4veenn <praveen7.info@gmail.com>
2025-01-27 16:53:58 +0530

    final commit

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -46,15 +46,6 @@ const adminAuth = (req, res, next) => {
     }
 }
 
-//error handling middleware
-// const errorHandlingMid = (err, req, res, next) => {
-//     console.error("Error ",err.message);
-
-//     res.status(err.status || 500).json({
-//         success: false,
-//         message: err.message || "Internal Server Error",
-//     });
-// };
 
 
 
