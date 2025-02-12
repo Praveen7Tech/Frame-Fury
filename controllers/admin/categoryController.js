057a9b9f0b12f0a4fed0973ff99e500d47c1e25b
Pr4veenn <praveen7.info@gmail.com>
2025-02-12 09:58:35 +0530

    category limit

--- controllers/admin/categoryController.js
+++ controllers/admin/categoryController.js
@@ -4,7 +4,7 @@ const Category = require("../../models/categorySchema");
 const categoryInfo = async (req, res) => {
     try {
         const page = parseInt(req.query.page) || 1;
-        const limit = 5
+        const limit = 10
         const skip = (page - 1) * limit
 
         const categoryData = await Category.find({})
